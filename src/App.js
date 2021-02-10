302b4b27b50a7b44e9756102fe40e16e857cfbbb
pooja tipale <poojatipale332@gmail.com>
2021-02-10 15:37:03 +0530

    dynamic

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import './pooja.css';
+import React,{useState} from 'react';
+function App()
+{
+const[myCondition,changeMyCondition]=useState(true);
+
+let myDesign = '';
+
+let style={ 
+  backgroundColor:'red',
+  color:"white",
+  margin:'50px'
+};
+ 
+
+
+
+
+
+const showMyDivFunc=()=>
+  {
+    changeMyCondition(true);
+  }
+
+const hideMyDivFunc=()=>
+  {
+    changeMyCondition(false);
+  }
+const ToggleMyDivFunc=()=>
+  {
+   let Mycondition2 = myCondition;
+   
+   changeMyCondition(!Mycondition2);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if(myCondition)
+{
+  myDesign=(
+    <div className ="main_div">
+    <h1>This Is DIV</h1>
+    </div>
+  );
+  style.backgroundColor="green";
+}
+else{
+  style.backgroundColor="red";
+}
+
+
+
+  return(
+    <div>
+{/* <button className="showBTN" onClick={showMyDivFunc}>Show Div</button>
+<button className="hideBTN" onClick={hideMyDivFunc}>Hide Div</button> */}
+<button className="my_align" onClick={ToggleMyDivFunc}>Toggle Button</button>
+
+
+{myDesign}
+
+    </div>
+  );
+
+}
+ export default App;
